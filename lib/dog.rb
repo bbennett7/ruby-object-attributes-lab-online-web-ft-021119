4e7325c0bb8e4dc90610ed5cca8f8faac0c81a15
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-19 20:18:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
   def initialize(name)
-    @name = name 
+    @name = name
   end
   
   def name 
@@ -9,13 +9,5 @@ class Dog
   
   def name=(dog_name)
     @name = dog_name 
-  end
-  
-  def breed 
-    @breed 
-  end
-  
-  def breed=(dog_breed)
-    @breed = dog_breed 
   end
 end
